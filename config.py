f9ee785ea983ee2b8d3a992fa9f2e4af53242a0d
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2026-07-04 19:46:42 +0600

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # কনফিগারেশন ভ্যারিয়েবলসমূহ
-TOKEN = "8756368847:AAETVrdPjZelNDKWyMWoIxpAO4iAKeJIftA"                  
+TOKEN = "8756368847:AAGnP1O0K7QGSjnpmrwVxcO8tNd0OTmxxMQ"                  
 API_ID = 28870226                              
 API_HASH = "a5b1ff3f75941649bf5bc159782f0f00"                
 SESSION_STRING = ""                            
